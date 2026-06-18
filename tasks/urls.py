ec4fe9332a20451326709bcf59c67b93cfc436a4
Irfan syamil <syxmilz05@gmail.com>
2026-06-18 22:16:00 +0800

    Delete tasks directory

--- tasks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
-    path('tasks/add/', views.task_create, name='task_create'),
-    path('tasks/edit/<int:task_id>/', views.task_update, name='task_update'),
-    path('tasks/delete/<int:task_id>/', views.task_delete, name='task_delete'),
-]
